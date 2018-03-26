938023093bf4b6907b4dbf3a4e203bc5211237f2
David Cook <divergentdave@gmail.com>
2018-03-26 18:46:43 -0500

    Add test for parsing DER file

--- tests/test_carve.rs
+++ tests/test_carve.rs
@@ -21,3 +21,12 @@ fn test_load_zip_chain() {
     let certs = carver.carve_file(&mut stream);
     assert!(certs.len() == 2);
 }
+
+#[test]
+fn test_load_der_cert() {
+    let bytes = include_bytes!("files/davidsherenowitsa.party/cert.der");
+    let mut stream = Cursor::new(&bytes[..]);
+    let carver = Carver::new();
+    let certs = carver.carve_file(&mut stream);
+    assert!(certs.len() == 1);
+}
