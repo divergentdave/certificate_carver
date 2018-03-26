32d378c5f80bb28f22b0906cdfe480f7870f2c3c
David Cook <divergentdave@gmail.com>
2018-03-25 21:06:16 -0500

    Refactor test

--- tests/test_carve.rs
+++ tests/test_carve.rs
@@ -8,16 +8,16 @@ use std::io::Cursor;
 fn test_load_pem_chain() {
     let bytes = include_bytes!("files/davidsherenowitsa.party/fullchain.pem");
     let mut stream = Cursor::new(&bytes[..]);
-    let mut carver = Carver::new();
-    carver.scan_file(&mut stream, "fullchain.pem");
-    assert!(carver.map.len() == 2);
+    let carver = Carver::new();
+    let certs = carver.carve_file(&mut stream);
+    assert!(certs.len() == 2);
 }
 
 #[test]
 fn test_load_zip_chain() {
     let bytes = include_bytes!("files/davidsherenowitsa.party/fullchain.zip");
     let mut stream = Cursor::new(&bytes[..]);
-    let mut carver = Carver::new();
-    carver.scan_file(&mut stream, "fullchain.zip");
-    assert!(carver.map.len() == 2);
+    let carver = Carver::new();
+    let certs = carver.carve_file(&mut stream);
+    assert!(certs.len() == 2);
 }
