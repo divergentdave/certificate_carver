a058531cfc6a3181f49c19b34d10152558af7b2b
David Cook <divergentdave@gmail.com>
2018-03-25 21:55:19 -0500

    Improve handling of roots

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate regex;
 use std::env::args;
 use std::io::stdout;
 
-use certificate_carver::{Carver, LogInfo, CertificateFingerprint, check_crtsh, format_subject_issuer};
+use certificate_carver::{Carver, CertificateFingerprint, LogInfo, TrustRoots, check_crtsh, format_subject_issuer};
 
 const LOG_URLS: [&str; 1] = ["https://ct.googleapis.com/pilot/"];
 
@@ -23,23 +23,28 @@ fn main() {
     }
 
     let mut logs = Vec::new();
+    let mut all_roots = TrustRoots::new();
     for log_url in LOG_URLS.iter() {
         let mut log = LogInfo::new(log_url);
         log.roots = log.fetch_roots();
         for root_der in &log.roots[..] {
             carver.add_cert(root_der, "pilot roots");
         }
+        all_roots.add_roots(&log.roots);
         log.trust_roots.add_roots(&log.roots);
         logs.push(log);
     }
 
     let issuer_lookup = carver.build_issuer_lookup();
 
-    let total = carver.map.len();
     let mut total_found = 0;
     let mut total_not_found = 0;
     let mut new_fps: Vec<CertificateFingerprint> = Vec::new();
     for (fp, info) in carver.map.iter() {
+        if let Ok(_) = all_roots.test_fingerprint(fp) {
+            // skip root CAs
+            continue;
+        }
         let found = check_crtsh(fp).unwrap();
         format_subject_issuer(&info.cert, &mut stdout()).unwrap();
         println!("");
@@ -49,9 +54,10 @@ fn main() {
             total_found += 1;
         } else {
             total_not_found += 1;
+            new_fps.push(fp.clone());
         }
-        new_fps.push(fp.clone());
     }
+    let total = total_found + total_not_found;
     println!("{}/{} in crt.sh already, {}/{} not yet in crt.sh", total_found, total, total_not_found, total);
 
     for fp in new_fps.iter() {
