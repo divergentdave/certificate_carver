9c9fddad1034406155083dc967e9e7b640d5e425
David Cook <divergentdave@gmail.com>
2018-03-26 19:39:31 -0500

    Disable cross-compilation (due to openssl-sys)

--- ci/install.sh
+++ ci/install.sh
@@ -5,13 +5,9 @@ main() {
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-musl
         sort=sort
-
-        sudo apt-get install pkg-config libssl-dev
     else
         target=x86_64-apple-darwin
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
-
-        brew install openssl
     fi
 
     # Builds for iOS are done on OSX, but require the specific target to be
