7ea23b53816b99e2c710a66ce33eab54d2e03b4c
David Cook <divergentdave@gmail.com>
2019-01-09 00:12:48 -0600

    Add issued to reftest checks

--- src/bin/reftest.rs
+++ src/bin/reftest.rs
@@ -1,3 +1,5 @@
+extern crate openssl;
+
 extern crate certificate_carver;
 
 use std::env::args;
@@ -38,5 +40,31 @@ fn main() {
             mismatch_count += 1;
         }
     }
-    println!("{} certificates matched, {} certificates didn't match", match_count, mismatch_count);
+    println!("Names: {} certificates matched, {} certificates didn't match", match_count, mismatch_count);
+
+    let mut match_count = 0;
+    let mut mismatch_count = 0;
+    for (_fp, info1) in carver.map.iter() {
+        for (_fp, info2) in carver.map.iter() {
+            let issued_old = info1.cert.issued(&info2.cert) == openssl::x509::X509VerifyResult::OK;
+
+            let cert1_new = x509::CertificateInfo::new(info1.der.0.clone()).unwrap();
+            let cert2_new = x509::CertificateInfo::new(info2.der.0.clone()).unwrap();
+            let issued_new = cert1_new.issued(&cert2_new);
+
+            if issued_old == issued_new {
+                match_count += 1;
+            } else {
+                let mut name1: Vec<u8> = Vec::new();
+                let mut name2: Vec<u8> = Vec::new();
+                cert1_new.format_issuer_subject(&mut name1).unwrap();
+                cert2_new.format_issuer_subject(&mut name2).unwrap();
+                let name1 = String::from_utf8(name1).unwrap();
+                let name2 = String::from_utf8(name2).unwrap();
+                println!("Did {} issue {}? OpenSSL says {}, new code says {}", name1, name2, issued_old, issued_new);
+                mismatch_count += 1;
+            }
+        }
+    }
+    println!("Issued: {} pairs matched, {} pairs didn't match", match_count, mismatch_count);
 }
