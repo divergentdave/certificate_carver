2cc8f1e3735d116c8f07d8086a28a4acf2065d7f
David Cook <divergentdave@gmail.com>
2019-03-02 15:01:33 -0600

    Add test for carving at different file offsets

---
+++ tests/test_offset.rs
@@ -0,0 +1,47 @@
+extern crate certificate_carver;
+
+use std::io::Cursor;
+
+use certificate_carver::Carver;
+
+const MAX_PADDING: usize = 10 * 1024 * 1024;
+const PADDINGS: [usize; 8] = [
+    0,
+    1,
+    512,
+    1024,
+    1024 * 1024,
+    5 * 1024 * 1024,
+    10 * 1024 * 1024 - 1,
+    10 * 1024 * 1024,
+];
+
+#[test]
+fn test_offset_pem_cert() {
+    let bytes = include_bytes!("files/davidsherenowitsa.party/fullchain.pem");
+    let mut padded = vec![0; MAX_PADDING];
+    padded.extend_from_slice(bytes);
+    let padded = padded;
+    let carver = Carver::new(Vec::new());
+    for padding in PADDINGS.iter() {
+        let offset: usize = MAX_PADDING - *padding;
+        let mut stream = Cursor::new(&padded[offset..]);
+        let certs = carver.carve_file(&mut stream);
+        assert!(certs.len() == 2);
+    }
+}
+
+#[test]
+fn test_offset_der_cert() {
+    let bytes = include_bytes!("files/davidsherenowitsa.party/cert.der");
+    let mut padded = vec![0; MAX_PADDING];
+    padded.extend_from_slice(bytes);
+    let padded = padded;
+    let carver = Carver::new(Vec::new());
+    for padding in PADDINGS.iter() {
+        let offset: usize = MAX_PADDING - *padding;
+        let mut stream = Cursor::new(&padded[offset..]);
+        let certs = carver.carve_file(&mut stream);
+        assert!(certs.len() == 1);
+    }
+}
