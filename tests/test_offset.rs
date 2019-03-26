4c4976d0cbb16e46d4007d4e95c1617d76b2b69c
David Cook <divergentdave@gmail.com>
2019-03-25 19:00:01 -0500

    Test different offsets between certificates

--- tests/test_offset.rs
+++ tests/test_offset.rs
@@ -45,3 +45,59 @@ fn test_offset_der_cert() {
         assert_eq!(certs.len(), 1, "padding is {}", padding);
     }
 }
+
+#[test]
+fn test_pem_then_der() {
+    let pem_bytes = include_bytes!("files/davidsherenowitsa.party/fullchain.pem");
+    let der_bytes = include_bytes!("files/davidsherenowitsa.party/cert.der");
+    let mut vec1 = vec![0; MAX_PADDING];
+    vec1.extend_from_slice(pem_bytes);
+    let vec1 = vec1;
+    let zeros = &vec1[..MAX_PADDING];
+    let carver = Carver::new(Vec::new());
+    for padding_infix in PADDINGS.iter() {
+        let mut vec2 = vec1.clone();
+        vec2.extend_from_slice(&zeros[..*padding_infix]);
+        vec2.extend_from_slice(der_bytes);
+        for padding_prefix in PADDINGS.iter() {
+            let offset: usize = MAX_PADDING - *padding_prefix;
+            let mut stream = Cursor::new(&vec2[offset..]);
+            let certs = carver.carve_file(&mut stream);
+            assert_eq!(
+                certs.len(),
+                3,
+                "prefix padding is {}, infix padding is {}",
+                padding_prefix,
+                padding_infix
+            );
+        }
+    }
+}
+
+#[test]
+fn test_der_then_pem() {
+    let der_bytes = include_bytes!("files/davidsherenowitsa.party/cert.der");
+    let pem_bytes = include_bytes!("files/davidsherenowitsa.party/fullchain.pem");
+    let mut vec1 = vec![0; MAX_PADDING];
+    vec1.extend_from_slice(der_bytes);
+    let vec1 = vec1;
+    let zeros = &vec1[..MAX_PADDING];
+    let carver = Carver::new(Vec::new());
+    for padding_infix in PADDINGS.iter() {
+        let mut vec2 = vec1.clone();
+        vec2.extend_from_slice(&zeros[..*padding_infix]);
+        vec2.extend_from_slice(pem_bytes);
+        for padding_prefix in PADDINGS.iter() {
+            let offset: usize = MAX_PADDING - *padding_prefix;
+            let mut stream = Cursor::new(&vec2[offset..]);
+            let certs = carver.carve_file(&mut stream);
+            assert_eq!(
+                certs.len(),
+                3,
+                "prefix padding is {}, infix padding is {}",
+                padding_prefix,
+                padding_infix
+            );
+        }
+    }
+}
