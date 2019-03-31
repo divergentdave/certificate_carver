dc027a8672b6dd0e6a095e5476dc3c412852f679
David Cook <divergentdave@gmail.com>
2019-03-31 14:40:52 -0500

    Consolidate identical files for test harnesses

--- tests/mocks.rs
+++ tests/mocks.rs
@@ -35,20 +35,20 @@ impl LogServers for MockLogServers {
     fn fetch_roots_resp(&self, log: &LogInfo) -> Result<GetRootsResponse, APIError> {
         let url_str = log.get_url().as_str();
         let json_str;
-        if url_str == "https://ct.googleapis.com/pilot/" {
-            json_str = include_str!("roots/pilot.json");
-        } else if url_str == "https://ct.googleapis.com/daedalus/" {
-            json_str = include_str!("roots/daedalus.json");
+        if (url_str == "https://ct.googleapis.com/pilot/")
+            || (url_str == "https://ct.googleapis.com/daedalus/")
+        {
+            json_str = include_str!("roots/pilot-daedalus.json");
         } else if url_str == "https://ct.googleapis.com/icarus/" {
             json_str = include_str!("roots/icarus.json");
         } else if url_str == "https://ct1.digicert-ct.com/log/" {
             json_str = include_str!("roots/digicert-ct1.json");
         } else if url_str == "https://dodo.ct.comodo.com/" {
             json_str = include_str!("roots/dodo.json");
-        } else if url_str == "https://sabre.ct.comodo.com/" {
-            json_str = include_str!("roots/sabre.json");
-        } else if url_str == "https://mammoth.ct.comodo.com/" {
-            json_str = include_str!("roots/mammoth.json");
+        } else if (url_str == "https://sabre.ct.comodo.com/")
+            || (url_str == "https://mammoth.ct.comodo.com/")
+        {
+            json_str = include_str!("roots/mammoth-sabre.json");
         } else if url_str == "https://plausible.ct.nordu.net/" {
             json_str = include_str!("roots/plausible.json");
         } else {
