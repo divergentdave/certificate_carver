4a8574ef49a5138d2c2858085984fb288ec3bc1b
David Cook <divergentdave@gmail.com>
2019-03-31 20:50:02 -0500

    Add more sharded logs

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ const DIGICERT_CT1_ROOTS: &str = include_str!("../roots/digicert-ct1.json");
 const DODO_ROOTS: &str = include_str!("../roots/dodo.json");
 const MAMMOTH_SABRE_ROOTS: &str = include_str!("../roots/mammoth-sabre.json");
 const PLAUSIBLE_ROOTS: &str = include_str!("../roots/plausible.json");
-const ARGON_ROOTS: &str = include_str!("../roots/argon.json");
+const ARGON_XENON_ROOTS: &str = include_str!("../roots/argon-xenon.json");
 const NIMBUS_ROOTS: &str = include_str!("../roots/nimbus.json");
+const NESSIE_YETI_ROOTS: &str = include_str!("../roots/nessie-yeti.json");
 
 fn main() {
     let mut iter = args();
@@ -61,32 +62,57 @@ fn main() {
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2017/",
             LogShard::ExpiryYear(2017),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2018/",
             LogShard::ExpiryYear(2018),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2019/",
             LogShard::ExpiryYear(2019),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2020/",
             LogShard::ExpiryYear(2020),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2021/",
             LogShard::ExpiryYear(2021),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.googleapis.com/logs/argon2022/",
             LogShard::ExpiryYear(2022),
-            ARGON_ROOTS,
+            ARGON_XENON_ROOTS,
+        ),
+        LogInfo::new(
+            "https://ct.googleapis.com/logs/xenon2018/",
+            LogShard::ExpiryYear(2018),
+            ARGON_XENON_ROOTS,
+        ),
+        LogInfo::new(
+            "https://ct.googleapis.com/logs/xenon2019/",
+            LogShard::ExpiryYear(2019),
+            ARGON_XENON_ROOTS,
+        ),
+        LogInfo::new(
+            "https://ct.googleapis.com/logs/xenon2020/",
+            LogShard::ExpiryYear(2020),
+            ARGON_XENON_ROOTS,
+        ),
+        LogInfo::new(
+            "https://ct.googleapis.com/logs/xenon2021/",
+            LogShard::ExpiryYear(2021),
+            ARGON_XENON_ROOTS,
+        ),
+        LogInfo::new(
+            "https://ct.googleapis.com/logs/xenon2022/",
+            LogShard::ExpiryYear(2022),
+            ARGON_XENON_ROOTS,
         ),
         LogInfo::new(
             "https://ct.cloudflare.com/logs/nimbus2017/",
@@ -122,6 +148,56 @@ fn main() {
             "https://ct.cloudflare.com/logs/nimbus2023/",
             LogShard::ExpiryYear(2023),
             NIMBUS_ROOTS,
+        ),
+        LogInfo::new(
+            "https://nessie2018.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2018),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://nessie2019.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2019),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://nessie2020.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2020),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://nessie2021.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2021),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://nessie2022.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2022),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://yeti2018.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2018),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://yeti2019.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2019),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://yeti2020.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2020),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://yeti2021.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2021),
+            NESSIE_YETI_ROOTS,
+        ),
+        LogInfo::new(
+            "https://yeti2022.ct.digicert.com/log/",
+            LogShard::ExpiryYear(2022),
+            NESSIE_YETI_ROOTS,
         ),
     ];
     let mut carver = Carver::new(logs);
