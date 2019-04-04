cab64a67331595ae17eb7bac28812de54bd7dc51
David Cook <divergentdave@gmail.com>
2019-04-03 19:31:09 -0500

    Add script to re-download log roots

---
+++ roots/download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+curl https://ct.googleapis.com/logs/argon2017/ct/v1/get-roots > argon-xenon.json
+curl https://ct1.digicert-ct.com/log/ct/v1/get-roots > digicert-ct1.json
+curl https://dodo.ct.comodo.com/ct/v1/get-roots > dodo.json
+curl https://ct.googleapis.com/icarus/ct/v1/get-roots > icarus.json
+curl https://mammoth.ct.comodo.com/ct/v1/get-roots > mammoth-sabre.json
+curl https://nessie2018.ct.digicert.com/log/ct/v1/get-roots > nessie-yeti.json
+curl https://ct.cloudflare.com/logs/nimbus2017/ct/v1/get-roots > nimbus.json
+curl https://ct.googleapis.com/pilot/ct/v1/get-roots > pilot-daedalus.json
+curl https://plausible.ct.nordu.net/ct/v1/get-roots > plausible.json
