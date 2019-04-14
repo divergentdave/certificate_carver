05fbcb0dcebccbec75b156e02303494dd2f4fbfd
David Cook <divergentdave@gmail.com>
2019-04-14 13:33:34 -0500

    Reorganize tests

--- src/lib.rs
+++ src/lib.rs
@@ -572,3 +572,32 @@ impl CrtShServer for RealCrtShServer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Carver;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_der_too_short() {
+        const BYTES: [u8; 14] = [
+            0x30, 0x82, 0xff, 0xff, 0x30, 0x82, 0xff, 0xf0, 0xa0, 0x03, 0x02, 0x01, 0x02, 0x02,
+        ];
+
+        let mut stream = Cursor::new(&BYTES);
+        let carver = Carver::new(Vec::new());
+        let certs = carver.carve_stream(&mut stream);
+        assert!(certs.is_empty());
+    }
+
+    #[test]
+    fn test_pem_too_short() {
+        const BYTES: &[u8] = b"-----BEGIN CERTIFICATE-----\nMIIC";
+
+        let mut stream = Cursor::new(&BYTES);
+        let carver = Carver::new(Vec::new());
+        let certs = carver.carve_stream(&mut stream);
+        assert!(certs.is_empty());
+    }
+
+}
