3a415d55e387938281c8e3590c3c90ae797d7ab1
David Cook <divergentdave@gmail.com>
2019-05-17 20:54:24 -0500

    Handle API changes to base64
    The base64 crate no longer supports line wrapping nor whitespace
    stripping.
    
    Previously, chains that were submitted to CT logs had PEM-style line
    wrapping of the base64-encoded certificates, which was unusual, but
    still worked with most logs. It seems add-chain payloads with no
    newlines in base64 strings is the norm.
    
    Whitespace stripping was removed in alicemaz/rust-base64#81. This commit
    copies the relevant removed function, and strips whitespace before
    passing base64 data to be decoded.

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -4,6 +4,14 @@ extern crate certificate_carver;
 
 use certificate_carver::CertificateBytes;
 
+// This was removed from base64 in version 0.10.0
+fn copy_without_whitespace(input: &[u8]) -> Vec<u8> {
+    let mut input_copy = Vec::<u8>::with_capacity(input.len());
+    input_copy.extend(input.iter().filter(|b| !b" \n\t\r\x0b\x0c".contains(b)));
+
+    input_copy
+}
+
 pub fn decode_pem(pem_data: &[u8]) -> CertificateBytes {
     let mut pem_data = pem_data;
     if pem_data.ends_with(b"\n") {
@@ -14,11 +22,8 @@ pub fn decode_pem(pem_data: &[u8]) -> CertificateBytes {
     assert!(pem_data.starts_with(prefix));
     assert!(pem_data.ends_with(suffix));
     let base64_data = pem_data[prefix.len()..pem_data.len() - suffix.len()].to_vec();
-    let config = base64::Config::new(
-        base64::CharacterSet::Standard,
-        true,
-        true,
-        base64::LineWrap::Wrap(64, base64::LineEnding::CRLF),
-    );
-    CertificateBytes(base64::decode_config(&base64_data, config).unwrap())
+    let config = base64::Config::new(base64::CharacterSet::Standard, true);
+    CertificateBytes(
+        base64::decode_config(&copy_without_whitespace(base64_data.as_ref()), config).unwrap(),
+    )
 }
