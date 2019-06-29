27618b88e220402cdd955f7f687d75e4081b5c0e
David Cook <divergentdave@gmail.com>
2019-06-29 15:25:39 -0500

    Update cargo-fuzz directory to Rust 2018 edition

--- fuzz/fuzz_targets/fuzzer_carve.rs
+++ fuzz/fuzz_targets/fuzzer_carve.rs
@@ -1,6 +1,5 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate certificate_carver;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let carver = certificate_carver::Carver::new(Vec::new());
