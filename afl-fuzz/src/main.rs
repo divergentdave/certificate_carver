c5172fb95de0c0e16fe2626666084551ef94762e
David Cook <divergentdave@gmail.com>
2019-07-27 10:08:15 -0500

    Add afl-fuzz target

---
+++ afl-fuzz/src/main.rs
@@ -0,0 +1,9 @@
+use afl::fuzz;
+
+fn main() {
+    fuzz!(|data: &[u8]| {
+        let mut file_carver = certificate_carver::FileCarver::new();
+        let mut cursor = std::io::Cursor::new(data);
+        file_carver.carve_stream(&mut cursor);
+    });
+}
